bd8ae3bdc2a2bcffae51053533c1f69057c8c3f7
gabriM <g.manenti013@studenti.unibs.it>
2019-03-12 17:24:26 +0100

    Add files via upload

---
+++ Evento.java
@@ -0,0 +1,20 @@
+public class Evento {
+	
+	//Attributi
+	private Categoria categoria;
+	private Boolean validità= false;
+	
+	
+	//Costruttori
+	public Evento(Categoria _categoria){
+		categoria= _categoria;
+	}
+	
+	//Metodi
+	
+	
+	
+	// Getters and Setters generati automaticamente
+	
+
+}
---
+++ Valore.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+public class Valore {
+	
+	//Attributi
+	private String testo;
+	private int numero;
+	private Date giorno;
+	
+	
+	//Costruttori
+	public Valore(String _testo){
+		testo=_testo;
+	}
+	public Valore(int _numero){
+		numero = _numero;
+	}
+	public Valore(Date _giorno){
+		giorno = _giorno;
+	}
+	
+	//Metodi
+	
+	
+	
+	// Getters and Setters generati automaticamente
+	
+
+}
